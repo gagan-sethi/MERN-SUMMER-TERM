fe8bbe0f0dd8a20ce8fe304f086d48bb7c3635a7
ravi-sethi <ravisethi7102@gmail.com>
2026-06-25 19:50:40 +0530

    Fixed bug while adding same item again

--- key-shop/src/App.jsx
+++ key-shop/src/App.jsx
@@ -14,7 +14,6 @@ import './App.css';
 function App(){
  // const [cartCount, setCartCount]=useState(0);
  const [cartItems, setCartItems] = useState([]);
-
   function addToCart(product){
     if(!product){
       return;
@@ -23,15 +22,17 @@ function App(){
     const productAlreadyInCart= previousCartItems.find((item)=>item.id==product.id);
 
       if(productAlreadyInCart){
+        
         return previousCartItems.map((item)=>{
-          item.id==product.id ? {...item, quantity: item.quantity +1 } : item;
+          return item.id==product.id ? {...item, quantity: item.quantity + 1 } : item;
         });
       } 
       return [...previousCartItems, {...product, quantity: 1}];
     })
   }
-
-  const cartCount = cartItems.reduce((total, item)=>total+item.quantity, 0);
+ 
+  const cartCount = cartItems.reduce((total, item)=> total + item.quantity, 0);
+  
 
   return (
     <BrowserRouter>
